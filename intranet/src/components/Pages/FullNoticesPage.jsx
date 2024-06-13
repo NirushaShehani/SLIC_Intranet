f338cbc6f7975f1cce7814b60a77d03f2b3864be
Sachini Sarathchandra <it21213458@my.sliit.lk>
2024-06-13 11:09:22 +0530

    full notices page added

--- intranet/src/components/Pages/FullNoticesPage.jsx
+++ intranet/src/components/Pages/FullNoticesPage.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import KnowledgeBase from '../Home_Components/KnowledgeBase';
 import CompanyEvents from '../Sub_Components/HRNotices';
 import BranchPerformance from '../Home_Components/BranchPerformance';
-import '../../Styles/Home.css';
+import '../../Styles/FullNoticesPage.css';
 
 const FullNoticesPage = () => {
   return (
-    <div className="home">
-      <div className="home-column1">
+    <div className="full-notices-page">
+      <div className="full-notices-column1">
         <KnowledgeBase />
       </div>
       <hr />
-      <div className="home-column2">
+      <div className="full-notices-column2">
         <CompanyEvents />
       </div>
       <hr />
-      <div className="home-column4">
+      <div className="full-notices-column4">
         <BranchPerformance />
       </div>
     </div>
