7fad690909fab2e7107b0f22435397aff3942db7
Sachini Sarathchandra <it21213458@my.sliit.lk>
2024-06-13 10:04:12 +0530

    changed size of the CompanyEvents

--- intranet/src/components/Home_Components/CompanyEvents.jsx
+++ intranet/src/components/Home_Components/CompanyEvents.jsx
@@ -8,20 +8,20 @@ import { Link } from 'react-router-dom';
 const CompanyEvents = () => {
   return (
     <div className="CompanyEvents">
-      <h2 className="notice-head">CompanyEvents & News</h2>
+      <h2 className="notice-hd">CompanyEvents & News</h2>
       <div className="image-container">
         <img src={image1} alt="Image 1" className="image image1" />
         <img src={image2} alt="Image 2" className="image image2" />
         <img src={image3} alt="Image 3" className="image image3" />
       </div>
       <h2 className="notice-head">HR Notices</h2>
-      <p className="sub-notice-head">SLICL Suba Pathum scholarship awards 2023</p>
+      <p className="sub-notice-hd">SLICL Suba Pathum scholarship awards 2023</p>
       <p className='notice'>Lorem Ipsum has been the industry's standard when it dummy text ever since 1500s when an unknown have took a galley of type.</p>
       <hr />
-      <p className="sub-notice-head">SLICL Suba Pathum scholarship awards 2023</p>
+      <p className="sub-notice-hd">SLICL Suba Pathum scholarship awards 2023</p>
       <p className='notice'>Lorem Ipsum has been the industry's standard car van dummy text ever since 1500s when an unknown when took a galley of type.</p>
       <hr />
-      <p className="sub-notice-head">SLICL Suba Pathum scholarship awards 2023</p>
+      <p className="sub-notice-hd">SLICL Suba Pathum scholarship awards 2023</p>
       <p className='notice'>Lorem Ipsum has been the industry's standard when it dummy text ever since 1500s when an unknown have took a galley of type.</p>
       <div className="link-container">
         <Link to="/full-notices" className="read-more-link">Read more...</Link>
