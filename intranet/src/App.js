70bf888e3749309ce990f09e48e45e842a0e5900
Nadeeshan Devinda Karunarathna <nadeeshanenterprises20@gmail.com>
2024-08-01 16:30:16 +0530

    Admin module removed

--- intranet/src/App.js
+++ intranet/src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Admin from './components/Admin'
 import FullNoticesPage from './components/Pages/FullNoticesPage';
-import DetailedEventsPageAdmin from './components/Admin_Components/AdminEventsAndComponents/DetailedEventsPageAdmin';
 import DetailedEventsPage from './components/Pages/DetailedEventsPage';
 import DetailedNoticePage from './components/Pages/DetailedNoticePage';
 import EventsPage from './components/Pages/EventsPage';
@@ -16,11 +14,7 @@ import FullBranchEvent from './components/Pages/FullBranchEvent';
 import UploadEvent from './components/Pages/UploadEvent';
 import ServicesPage from './components/Pages/Services'
 import MediaUploadDetails from './components/Pages/MediaUploadDetails';
-import HRNoticeAdmin1 from './components/Admin_Components/HRNoticeAdmin1';
-import HRNoticeAdmin2 from './components/Admin_Components/HRNoticeAdmin2';
 import AllNotices from './components/Pages/AllNotices';
-import EventsAdminPage from './components/Admin_Components/AdminEventsAndComponents/EventsAdminPage';
-import FullNoticesAdmin from './components/Admin_Components/AdminHRNotices/FullNoticesAdmin';
 import SalesLead from './components/Pages/SalesLeadPage';
 import CostCenter from './components/Pages/Cost_Center';
 import IdeaHub from './components/Pages/IdeaHub';
@@ -32,7 +26,6 @@ const App =() => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
         <Route path="/full-notices" element={<FullNoticesPage />} />
         <Route path="/detailed-notice/:id" element={<DetailedNoticePage />} />
         <Route path="/detailed-events/:id" element={<DetailedEventsPage />} />
@@ -50,16 +43,6 @@ const App =() => {
         <Route path="/Idea-Hub" element={<IdeaHub/>}/>
         <Route path="/Knowledge-Base" element={<Knowledge_BasePage/>}/>
         <Route path="/Book-Hub" element={<BookHub/>}/>
-        
-        {/* admin side */}
-        <Route path="/events-admin" element={<EventsAdminPage />} />
-        <Route path="/hr-notice-admin1" element={<HRNoticeAdmin1 />} />
-        <Route path="/hr-notice-admin2" element={<HRNoticeAdmin2 />} />
-        <Route path="/upload-event" element={<UploadEvent />} />
-        <Route path="/detailed-events-admin/:id" element={<DetailedEventsPageAdmin />} />
-        <Route path="/full-notices-admin" element={<FullNoticesAdmin />} />
-       
-
       </Routes>
     </Router>
   );
