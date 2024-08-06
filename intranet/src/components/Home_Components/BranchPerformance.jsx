62416355d9387db01dd0cf371187e3e16b66b3c1
Nadeeshan Devinda <73753128+NadeeshaDevin@users.noreply.github.com>
2024-08-06 15:40:29 +0530

    Branch performance pages updated

--- intranet/src/components/Home_Components/BranchPerformance.jsx
+++ intranet/src/components/Home_Components/BranchPerformance.jsx
@@ -7,8 +7,13 @@ import image3 from '../../assets/Event3.jpg';
 import { Link } from 'react-router-dom';
 
 const branches = [
- 
- 
+  { name: 'Kandy', value: 5000000, target: 4500000 },
+  { name: 'Panadura', value: 4600000, target: 4000000 },
+  { name: 'Galle', value: 4000000, target: 3000000 },
+  { name: 'Bandarawela', value: 3000000, target: 3500000 },
+  { name: 'Aluthgama', value: 2500000, target: 2000000 },
+  { name: 'Akurassa', value: 1700000, target: 1800000 },
+  { name: 'Dehiwala', value: 1000000, target: 1200000 },
 ];
 
 // Separate branches into two lists: achieved and not achieved
@@ -55,27 +60,6 @@ const BranchPerformance = () => {
         <Link to="/full-branch-performance" className="read-more">Read more...</Link>
       </div>
         {/*  */}
-        <h2>Cost Center</h2>
-        <div className="branch-list">
-          {achievedBranches.map((branch, index) => (
-            <div key={index} className="branch-item achieved">
-              <span>{branch.name}</span>
-              <span>Rs. {branch.value.toLocaleString('en-US', { minimumFractionDigits: 2 })}</span>
-            </div>
-          ))}
-        </div>
-        <div className="branch-list">
-          {notAchievedBranches.map((branch, index) => (
-            <div key={index} className="branch-item">
-              <span>{branch.name}</span>
-              <span>Rs. {branch.value.toLocaleString('en-US', { minimumFractionDigits: 2 })}</span>
-            </div>
-          ))}
-        </div>
-        {/* <div className="read-more">Read more...</div> */}
-        <div className="link-contain">
-        <Link to="/full-branch-performance" className="read-more">Read more...</Link>
-      </div>
       </div>
       <div className="branch-event">
         <h2>Branch Event</h2>
